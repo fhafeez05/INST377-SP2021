295e0301d1de12dca721715f25c2ae8cb2ee4b96
fhafeez05 <fhafeez05@users.noreply.github.com>
2021-02-12 21:13:42 -0500

    lab 3 errors and failures cannot get slider to slide

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,21 @@
-/* Put your javascript in here */
+const carouselImages = document.querySelector('.carousel_images');
+const carouselButtons = document.querySelectorAll('.carousel_button');
+const numberOfImages = document.querySelectorAll('.carousel_images img').length;
+let imageIndex = 1;
+let translateX = 0;
+ 
+carouselButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    if (event.target.id === 'previous') {
+      if (imageIndex !== 1) {
+        imageIndex--;
+        translateX += 300;
+      }
+    } elseif (imageIndex !== numberOfImages) {
+            imageIndex++;
+            translateX -= 300;
+      }
+    }
+       carouselImages.style.transform = `translateX(${translateX}px)`
+   });
+ });
